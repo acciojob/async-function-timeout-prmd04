57540593e8640a461a93e5aeab6d20aa660a9691
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-25 14:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let sbtBtn = document.getElementById("btn");
+
+sbtBtn.addEventListener('click', ()=>{
+  let textInput = document.getElementById("text").value.trim();
+  let delay = parseInt(document.getElementById('delay').value);
+
+  output(textInput,delay);
+})
+
+async function output(text,time){
+  await new Promise((resolve) =>{
+    setTimeout(()=>{
+      resolve();
+    },time)
+  })
+   document.getElementById("output").innerText = text;
+   document.getElementById("text").value='';
+   document.getElementById('delay').value='';
+
+};
